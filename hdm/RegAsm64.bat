4c928cfebab63940fa085602ad6d7a1b153ac3ac
Shawal Ahmad Mohmand <criticalmiind@gmail.com>
2025-08-30 13:15:55 +0400

    changes

---
+++ hdm/RegAsm64.bat
@@ -0,0 +1,4 @@
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe "%~dp0\HDMPrint.dll" /codebase /tlb 
+C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe "%~dp0\HDMPrint.dll" /codebase /tlb 
+pause >nul
+
